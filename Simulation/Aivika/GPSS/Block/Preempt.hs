d36e117f7179811fb1cef772f3f7869412420575
David Sorokin <david.sorokin@gmail.com>
2017-03-04 09:24:03 +0300

    Updated the comments.

--- Simulation/Aivika/GPSS/Block/Preempt.hs
+++ Simulation/Aivika/GPSS/Block/Preempt.hs
@@ -27,7 +27,7 @@ data PreemptBlockMode a =
                      -- ^ the Priority mode; otherwise, the Interrupt mode
                      preemptBlockTransfer :: Maybe (Maybe Double -> Block (Transact a) ()),
                      -- ^ where to transfer the preempted transact,
-                     -- passing in the remaining time in the ADVANCE block
+                     -- passing in the remaining time from the ADVANCE block
                      preemptBlockRemoveMode :: Bool
                      -- ^ the Remove mode
                    }
