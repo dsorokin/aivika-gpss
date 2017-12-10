ac3e5928f65febffad987a82073622bbed3ec428
David Sorokin <david.sorokin@gmail.com>
2017-12-10 11:44:12 +0300

    Added two tests.

---
+++ tests/Example2AThroughSignal.hs
@@ -0,0 +1,59 @@
+
+{-
+  GENERATE 18,6
+  QUEUE JOEQ
+  SEIZE JOE
+  DEPART JOEQ
+  ADVANCE 16,4
+  RELEASE JOE
+  TERMINATE
+
+  GENERATE 480
+  TERMINATE 1
+
+  START 1
+ -}
+
+import Control.Category
+import Control.Monad.Trans
+
+import Simulation.Aivika
+import Simulation.Aivika.GPSS
+import qualified Simulation.Aivika.GPSS.Queue as Q
+
+specs = Specs { spcStartTime = 0.0,
+                spcStopTime = 480.0,
+                spcDT = 1.0,
+                spcMethod = RungeKutta4,
+                spcGeneratorType = SimpleGenerator }
+        
+model :: Simulation Results
+model =
+  do joeq <- runEventInStartTime Q.newQueue
+     joe  <- runEventInStartTime newFacility
+
+     clientSignal <-
+       runCompositeInStartTime_ $
+       newRandomUniformSignal (18 - 6) (18 + 6)
+
+     let clients = signalGeneratorBlock0 clientSignal
+         chain   =
+           queueBlock joeq 1 >>>
+           seizeBlock joe >>>
+           departBlock joeq 1 >>>
+           advanceBlock (randomUniformProcess_ (16 - 4) (16 + 4)) >>>
+           releaseBlock joe >>>
+           terminateBlock
+
+     runProcessInStartTime $
+       runGeneratorBlock clients chain 
+  
+     return $
+       results
+       [resultSource "joeq" "The Joe queue" joeq,
+        resultSource "joe" "Joe" joe]
+  
+main =
+  printSimulationResultsInStopTime
+  printResultSourceInEnglish
+  model specs
---
+++ tests/Example2AThroughSignalTrans.hs
@@ -0,0 +1,63 @@
+
+{-
+  GENERATE 18,6
+  QUEUE JOEQ
+  SEIZE JOE
+  DEPART JOEQ
+  ADVANCE 16,4
+  RELEASE JOE
+  TERMINATE
+
+  GENERATE 480
+  TERMINATE 1
+
+  START 1
+ -}
+
+import Control.Category
+import Control.Monad.Trans
+
+import Simulation.Aivika.Trans
+import Simulation.Aivika.Trans.GPSS
+import qualified Simulation.Aivika.Trans.GPSS.Queue as Q
+
+import Simulation.Aivika.IO
+
+type DES = IO
+
+specs = Specs { spcStartTime = 0.0,
+                spcStopTime = 480.0,
+                spcDT = 1.0,
+                spcMethod = RungeKutta4,
+                spcGeneratorType = SimpleGenerator }
+        
+model :: Simulation DES (Results DES)
+model =
+  do joeq <- runEventInStartTime Q.newQueue
+     joe  <- runEventInStartTime newFacility
+
+     clientSignal <-
+       runCompositeInStartTime_ $
+       newRandomUniformSignal (18 - 6) (18 + 6)
+
+     let clients = signalGeneratorBlock0 clientSignal
+         chain   =
+           queueBlock joeq 1 >>>
+           seizeBlock joe >>>
+           departBlock joeq 1 >>>
+           advanceBlock (randomUniformProcess_ (16 - 4) (16 + 4)) >>>
+           releaseBlock joe >>>
+           terminateBlock
+
+     runProcessInStartTime $
+       runGeneratorBlock clients chain 
+  
+     return $
+       results
+       [resultSource "joeq" "The Joe queue" joeq,
+        resultSource "joe" "Joe" joe]
+  
+main =
+  printSimulationResultsInStopTime
+  printResultSourceInEnglish
+  model specs
